d2c812d48fe65fb256a8ce5d6555968ee4c48c0b
Kyle Verhoog <kyle@verhoog.ca>
2014-08-01 16:20:40 -0400

    added solutions to p1 and p2

---
+++ solutions/p1.py
@@ -0,0 +1,10 @@
+#If we list all the natural numbers below 10 that are multiples of 3 and 5
+#we get 3, 5, 6 and 9. The sum of the multiples is 23. 
+#Find the sum of all the multiples of 3 and 5 below 1000. 
+s = 0
+u = 1000
+for i in range(0,u):
+	if(i%3 == 0) or (i%5 == 0):
+		s = s + i
+print(s)
+
---
+++ solutions/p2.py
@@ -0,0 +1,15 @@
+#By considering the terms in the sequence whose values do not exceed four million,
+#find the sum of the even-valued terms
+
+n_inc = 1
+s = 0
+n = 1
+while (n < 4000000):
+	x = n
+	n = n + n_inc
+	n_inc = x
+	if n%2==0 and n < 4000000:
+		s = s + n
+print(s)
+	
+		
