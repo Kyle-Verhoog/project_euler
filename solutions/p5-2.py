87476200179c373e854587eda44ab805336ba244
Kyle Verhoog <kyle@verhoog.ca>
2014-08-01 23:31:41 -0400

    (somewhat better) solution to problem 5 added

---
+++ solutions/p5-2.py
@@ -0,0 +1,17 @@
+#Problem 5 (Attempt 2): Smallest Multiple
+#Kyle Verhoog August 1, 2014
+
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder
+
+#What is the smallest positive number that is evenly divisible by all the numbers from 1 to 20?
+x=20
+while True:
+	smallest_multiple = True
+	for i in range(1,21):
+		if x%i != 0:
+			smallest_multiple = False
+	if smallest_multiple == True:
+		print x
+		exit()
+	x = x+20
+#Ans: 322792560
