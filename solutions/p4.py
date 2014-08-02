069775f59652ac8f7c076c19c0ef0539b7aced72
Kyle Verhoog <kyle@verhoog.ca>
2014-08-01 22:19:53 -0400

    solution to problem 4 added

---
+++ solutions/p4.py
@@ -0,0 +1,26 @@
+#Problem 4: Largest Palindrome Product
+#Kyle Verhoog August 1, 2014
+
+#A palindrome number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99
+#Find the largest palindrome made from the product of two 3-digit numbers
+
+def reverse(s):
+	r = ""
+	for i in range(len(s)-1,-1,-1):
+		r = r + s[i]
+	return r
+
+digits = 3
+l = []
+for i in range(10**(digits-1),10**digits):
+	for x in range(10**(digits-1),10**digits):
+		s = str(i*x)
+		if s == reverse(s):
+			l.append(i*x)
+
+k = 0
+for i in range(0,len(l)-1):
+	if l[i] > k:
+		k = l[i]
+print(k)
+
