2558f506ea9ed5f34a897151723a5393a6ef122c
Kyle Verhoog <kyle@verhoog.ca>
2014-08-07 19:36:35 -0400

    FINALLY solution to problem 15 added

--- solutions/p15.py
+++ solutions/p15.py
@@ -3,10 +3,23 @@
 
 
 #function works for first couple lattice structures, breaks down after that :(
-def lattice(g):
-	n = 1
-	for i in range(1,g):
-		n += g*i
-	n *= 2
-	print(n) 
+# def lattice(g):
+# 	n = 1
+# 	for i in range(1,g):
+# 		n += g*i
+# 	n *= 2
+# 	print(n) 
 
+
+#Using Every other row of Pascal's Triangle yields the nth number of paths
+
+def fact(n):
+	if n == 1:
+		return 1
+	else:
+		return n*fact(n-1)
+def paths(g):
+	return (fact(2*g)) / (fact(g)**2)
+
+print(paths(20))
+#Ans: 137846528820 
